90a293b18c6f9497d38baa998eb14a80f2638e6b
Jackylx2008 <jackylx2008@hotmail.com>
2022-11-18 14:10:57 +0800

    Adjust motion

--- lua/config/hop.lua
+++ lua/config/hop.lua
@@ -5,30 +5,30 @@ function M.setup()
   require("hop").setup()
 
   -- keymap("", "s", "<cmd>:HopWord<cr>", {})
-
+  local opts = { noremap = true, silent = true }
   keymap(
     "",
     "f",
     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
-    {}
+    opts
   )
   keymap(
     "",
     "F",
     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
-    {}
+    opts
   )
   keymap(
     "",
     "t",
     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
-    {}
+    opts
   )
   keymap(
     "",
     "T",
     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
-    {}
+    opts
   )
 end
 
