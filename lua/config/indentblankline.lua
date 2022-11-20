b4735c51fdf065c2c1472fb53aef541500fdc4ee
Jackylx2008 <jackylx2008@hotmail.com>
2022-11-20 17:12:25 +0800

    Visual Mode Commnet whichkey setup failedt

--- lua/config/indentblankline.lua
+++ lua/config/indentblankline.lua
@@ -11,7 +11,8 @@ function M.setup()
 
   vim.opt.list = true
   vim.opt.listchars:append "space:⋅"
-  vim.opt.listchars:append "eol:↴"
+  -- vim.opt.listchars:append "eol:↴"
+
   require("indent_blankline").setup {
     space_char_blankline = " ",
     char_highlight_list = {
